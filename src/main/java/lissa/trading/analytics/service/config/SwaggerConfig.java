6c069ae7ab2ab9ba7225ba3664e7ee5dbd868b7b
Dmitry Grishin <kenpxrkasf@gmail.com>
2024-10-30 17:48:55 +0300

    feat(STOCK-34) added swagger

---
+++ src/main/java/lissa/trading/analytics/service/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package lissa.trading.analytics.service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API для сервиса аналитики трейдинг ассистента")
+                        .description("Этот API предоставляет методы для получения и анализа данных о торговых акциях, " +
+                                "включая индикаторы и исторические данные, функционал будет пополняться ")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Дмитрий")
+                                .url("https://t.me/kenpxrk")));
+    }
+
+    @Bean
+    public GroupedOpenApi userApi() {
+        return GroupedOpenApi.builder()
+                .group("analytics")
+                .pathsToMatch("/v1/analytics/**")
+                .build();
+    }
+}
+
--- src/main/java/lissa/trading/analytics/service/tinkoff/dto/TinkoffCandlesRequestDto.java
+++ src/main/java/lissa/trading/analytics/service/tinkoff/dto/TinkoffCandlesRequestDto.java
@@ -1,5 +1,6 @@
 package lissa.trading.analytics.service.tinkoff.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lissa.trading.analytics.service.dto.CandleInterval;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,12 +8,15 @@ import lombok.NoArgsConstructor;
 
 import java.time.OffsetDateTime;
 
+@Schema(description = "Данные необходимые для запроса свечей в тинькофф сервисе")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class TinkoffCandlesRequestDto {
+    @Schema(description = "UUID или figi инструмента в API Тинькофф")
     private String instrumentId;
     private OffsetDateTime from;
     private OffsetDateTime to;
+    @Schema(description = "Интервал между свечами", example = "CANDLE_INTERVAL_5_MIN")
     private CandleInterval interval;
 }
