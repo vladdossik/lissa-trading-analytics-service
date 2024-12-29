6277c2d0ffa1853657d865cc6de72f4a62580d17
Dmitry Grishin <kenpxrkasf@gmail.com>
2024-12-29 15:03:51 +0300

    feat(STOCK-49) added security for endpoints

--- src/main/java/lissa/trading/analytics/service/LissaTradingAnalyticsServiceApplication.java
+++ src/main/java/lissa/trading/analytics/service/LissaTradingAnalyticsServiceApplication.java
@@ -3,9 +3,11 @@ package lissa.trading.analytics.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableFeignClients
+@ComponentScan(basePackages = "lissa.trading")
 public class LissaTradingAnalyticsServiceApplication {
 
     public static void main(String[] args) {
