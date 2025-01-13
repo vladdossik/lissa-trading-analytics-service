03c227215d836688c07b4ee22adee1c7f9c1cec9
Dmitry Grishin <kenpxrkasf@gmail.com>
2025-01-13 18:06:28 +0300

    feat(STOCK-49) changed swagger path

--- src/main/java/lissa/trading/analytics/service/config/SwaggerConfig.java
+++ src/main/java/lissa/trading/analytics/service/config/SwaggerConfig.java
@@ -6,10 +6,12 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties
 public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
